97e2ee2e7d7fc593cc1049c9f90032384b3e7595
Tonygcm <guilherme.coelhomendes@gmail.com>
2024-03-14 11:01:24 -0300

    segundo commit

---
+++ Aula03/ExAula03.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios Aula3</title>
+</head>
+
+<body>
+
+    <h2>Input</h2>
+    <form action="" method="GET">
+        <label>pergunta: </label>
+        <input name="ano" type="text">
+
+        <button type="submit">Enviar</button>
+    </form>
+
+
+</body>
+
+</html>
+
+<?php
+
+    /* 
+    
+     - Faça um Programa que peça dois números e imprima o maior deles.
+     
+     - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ounegativo.
+     
+     - Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+        Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+     - Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+     
+     - Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+     a. A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+     b. A mensagem "Reprovado", se a média for menor do que sete;
+     c. A mensagem "Aprovado com Distinção", se a média for igual a dez.
+    
+    */
+
+?>
